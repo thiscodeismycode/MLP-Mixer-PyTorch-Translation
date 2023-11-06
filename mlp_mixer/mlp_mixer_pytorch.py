e3c34241585b02d589121f0ea554b0b8a53875ee
Hyeonjin Kim <peaceful@snu.ac.kr>
2023-11-06 16:56:48 +0900

    mlp mixer first draft done

--- mlp_mixer/mlp_mixer_pytorch.py
+++ mlp_mixer/mlp_mixer_pytorch.py
@@ -1,22 +1,13 @@
-import torch
-import torchvision
 import einops
 import numpy as np
 from torch import nn
-from einops.layers.torch import Rearrange
-import torchvision.transforms as transforms
 
-# hyperparameters
-"""batch_size: int = 16
-in_channels: int = 3
-image_size: int = 32
-patch_size: int = 4
-num_classes: int = 10
-dim: int = 16
-depth: int = 8
-token_dim: int = 16
-channel_dim: int = 16
-epochs: int = 10"""
+"""
+Original code written in FLAX/JAX
+https://arxiv.org/abs/2105.01601
+https://github.com/google-research/vision_transformer/blob/main/vit_jax/models_mixer.py 
+"""
+
 
 # blocks
 class MlpBlock(nn.Module):
@@ -28,19 +19,34 @@ class MlpBlock(nn.Module):
         dim = x.shape()[-1]
         # y = nn.Dense(output_dim)(input)
         # FLAX nn.Dense() is a linear transformation applied over the last dimension of the input.
-        y = nn.Linear(dim, self.mlp_dim)(x) # nn.Linear(input_dim, output_dim)(input)
+        y = nn.Linear(dim, self.mlp_dim)(x)  # nn.Linear(input_dim, output_dim)(input)
         y = nn.GELU()(y)
         # Back to the original shape
         y = nn.Linear(y.shape()[-1], x.shape()[-1])
         return y
+
 
 class MixerBlock(nn.Module):
     def __init(self, tokens_mlp_dim, channels_mlp_dim):
         super().__init__()
         self.tokens_mlp_dim: int = tokens_mlp_dim
         self.channels_mlp_dim: int = channels_mlp_dim
+        self.token_mixing = MlpBlock(tokens_mlp_dim)
+        self.channel_mixing = MlpBlock(channels_mlp_dim)
 
     def forward(self, x):
+        # FLAX LayerNorm does not change input shape!
+        y = nn.LayerNorm(x.shape())(x)
+        np.swapaxes(y, 1, 2)
+        y = self.token_mixing(y)
+        np.swapaxes(y, 1, 2)
+        x = x + y
+
+        y = nn.LayerNorm(x.shape())(x)
+        y = self.channel_mixing(y)
+        x = x + y
+
+        return x
 
 
 class MlpMixer(nn.Module):
@@ -52,3 +58,19 @@ class MlpMixer(nn.Module):
         self.hidden_dim: int = hidden_dim
         self.tokens_mlp_dim: int = tokens_mlp_dim
         self.channels_mlp_dim: int = channels_mlp_dim
+        self.mixer_block = MixerBlock(tokens_mlp_dim, channels_mlp_dim)
+
+    def forward(self, x):
+        s = self.patch_size
+        # FLAX nn.Conv(features, kernel_size, strides=1, padding='SAME')
+        # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=0)
+        x = nn.Conv2d(x.shape()[-1], self.hidden_dim, (s, s), stride=(s, s))(x)
+        x = einops.rearrange(x, 'n h w c -> n (h w ) c')
+        for _ in range(self.num_blocks):
+            x = self.mixer_block(x)
+        x = nn.LayerNorm(x.shape())(x)
+        x = np.mean(x, axis=1)
+        output_head = nn.Linear(x.shape()[-1], self.num_classes)
+        nn.init.zeros_(output_head.weight.data)
+        output = output_head(x)
+        return output
